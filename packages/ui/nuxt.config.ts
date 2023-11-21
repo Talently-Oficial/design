bca67f265bcffeca7f8022e1ff97f7964d34d9f0
Dilvermg <dilvermg@gmail.com>
2023-11-21 00:35:47 -0500

    chore(nuxt): remove import nuxt/config

--- packages/ui/nuxt.config.ts
+++ packages/ui/nuxt.config.ts
@@ -1,6 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt/config'
-
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
     devtools: {
