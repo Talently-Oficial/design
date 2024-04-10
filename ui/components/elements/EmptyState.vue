75b40d61c740338123802e75517144119df8d8e1
Dilvermg <dilvermg@gmail.com>
2024-04-10 10:30:37 -0500

    feat: add components ContentHtml and EmptyState

---
+++ ui/components/elements/EmptyState.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="text-center py-4 px-6">
+    <div
+      class="rounded-full flex items-center justify-center m-auto mb-4"
+      :class="[sizeCircle, colorCircle]"
+    >
+      <slot name="icon">
+        <Icon name="heroicons:exclamation-triangle" size="28" />
+      </slot>
+    </div>
+
+    <p v-if="title" class="text-gray-800 mb-2">
+      {{ title }}
+    </p>
+
+    <div><slot /></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sizeCircle: {
+      type: String,
+      default: "w-24 h-24",
+    },
+    colorCircle: {
+      type: String,
+      default: "bg-yellow-50 text-yellow-600 bg-opacity-50",
+    },
+    title: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+};
+</script>
