f510a31aa5cbfcdde501d0ce143e90902cfab9b4
dilverdev <dilvermg@gmail.com>
2024-05-28 14:06:34 -0500

    feat: empty component improvement

--- ui/components/elements/EmptyState.vue
+++ ui/components/elements/EmptyState.vue
@@ -1,34 +1,37 @@
 <template>
-  <div class="text-center py-4 px-6">
-    <div
-      class="rounded-full flex items-center justify-center m-auto mb-4"
-      :class="[sizeCircle, colorCircle]"
-    >
-      <slot name="icon">
-        <Icon name="heroicons:exclamation-triangle" size="28" />
+  <div class="text-center">
+    <div class="mb-5">
+      <slot name="graphic">
+        <div class="rounded-full flex items-center justify-center m-auto w-18 h-18 bg-primary-500 bg-opacity-10">
+            <UIcon
+                name="i-ic-twotone-inbox"
+                class="text-primary-500 w-6 h-6"
+            />
+        </div>
       </slot>
     </div>
 
-    <p v-if="title" class="text-gray-800 mb-2">
+    <h3 v-if="title" class="text-neutral-700 font-semibold mb-1 text-lg">
       {{ title }}
+    </h3>
+
+    <p v-if="description" class="text-gray-800 text-sm mb-2">
+      {{ description }}
     </p>
 
-    <div><slot /></div>
+    <slot />
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    sizeCircle: {
-      type: String,
-      default: "w-24 h-24",
-    },
-    colorCircle: {
-      type: String,
-      default: "bg-yellow-50 text-yellow-600 bg-opacity-50",
-    },
     title: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    description: {
       type: String,
       required: false,
       default: "",
