cbc598d088bf1833c93d48c7c983905457430d97
dilverdev <dilvermg@gmail.com>
2024-05-29 09:28:22 -0500

    feat: add support icon in component EmptyState

--- ui/components/elements/EmptyState.vue
+++ ui/components/elements/EmptyState.vue
@@ -2,40 +2,49 @@
   <div class="text-center">
     <div class="mb-5">
       <slot name="graphic">
-        <div class="rounded-full flex items-center justify-center m-auto w-18 h-18 bg-primary-500 bg-opacity-10">
-            <UIcon
-                name="i-ic-twotone-inbox"
-                class="text-primary-500 w-6 h-6"
-            />
+        <div
+            class="rounded-full flex items-center justify-center m-auto w-18 h-18 bg-opacity-10"
+            :class="color"
+        >
+          <UIcon :name="props.icon" class=" w-6 h-6"/>
         </div>
       </slot>
     </div>
 
-    <h3 v-if="title" class="text-neutral-700 font-semibold mb-1 text-lg">
-      {{ title }}
+    <h3 v-if="props.title" class="text-neutral-700 font-semibold mb-1 text-lg">
+      {{ props.title }}
     </h3>
 
-    <p v-if="description" class="text-gray-800 text-sm mb-2">
-      {{ description }}
+    <p v-if="props.description" class="text-gray-800 text-sm mb-2">
+      {{ props.description }}
     </p>
 
     <slot />
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    title: {
-      type: String,
-      required: false,
-      default: "",
-    },
-    description: {
-      type: String,
-      required: false,
-      default: "",
-    },
+<script setup lang="ts">
+
+const props = defineProps({
+  color: {
+    type: String,
+    required: false,
+    default: "bg-primary-500 text-primary-500",
   },
-};
+  icon: {
+    type: String,
+    required: false,
+    default: "i-ic-twotone-inbox",
+  },
+  title: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  description: {
+    type: String,
+    required: false,
+    default: "",
+  },
+})
 </script>
