24b43af2e4c5a51e3146733cfcb4836218cb7462
dilverdev <dilvermg@gmail.com>
2024-06-23 14:25:23 -0500

    feat(shared): add utils

---
+++ shared/utils/constants/offerSuggestions.ts
@@ -0,0 +1,20 @@
+export const LOW_AND_MIDDLE_POTENTIAL_SCORE =
+    'Tu oferta parece tener potencial para ser más atractiva para los talentos. Para convertirla en una propuesta más competitiva, te recomendamos implementar algunas mejoras. Estos ajustes no solo aumentarán las posibilidades de atraer a más candidatos, sino que también te ayudarán a optimizar tu proceso de reclutamiento, atrayendo postulantes de mayor calidad en plazos más breves.'
+
+export const SKILL_AND_LOW_SALARY =
+    'El interés en tu oferta puede estar siendo afectado por un alto requerimiento de habilidades técnicas combinado con una compensación salarial no acorde. Considera la posibilidad de ajustar el salario ofrecido hacia arriba para hacer tu propuesta más atractiva.'
+
+export const HIGH_YEARS_OF_EXPERIENCE =
+    'Si tu oferta está generando un interés bajo, podría deberse a los años de experiencia que estás solicitando. Ser más flexible con este criterio, reduciendo los años requeridos, puede ampliar tu alcance a un grupo más diverso de talentos.'
+
+export const ENGLISH_LEVEL_AND_SALARY =
+    'Un nivel de inglés exigente con una compensación salarial que no se percibe como adecuada puede limitar el interés en tu oferta. Evalúa incrementar el presupuesto o flexibilizar el nivel de ingles para equilibrar mejor estos requisitos.'
+
+export const AVERAGE_AFFINITY_LOG =
+    'Considera flexibilizar algunos de los requisitos técnicos o habilidades específicas para ampliar el espectro de candidatos potenciales.'
+
+export const LOW_TARGET =
+    'Amplía tu público objetivo ajustando la modalidad de empleo o incluyendo opciones de trabajo remoto para atraer a un mayor número de talentos.'
+
+export const LOW_SALARY =
+    'Tu salario está por debajo del promedio del mercado para un [rol]. Considera aumentar el salario ofrecido para mejorar la atracción de candidatos calificados.'
