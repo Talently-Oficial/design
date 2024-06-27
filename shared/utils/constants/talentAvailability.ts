2638962d5cc7366496a4e1809ea2666519638c05
dilverdev <dilvermg@gmail.com>
2024-06-27 16:36:26 -0500

    feat(shred): constants talent_availability

---
+++ shared/utils/constants/talentAvailability.ts
@@ -0,0 +1,15 @@
+export const ID_TALENT_AVAILABILITY_INTERVIEWS: number = 1
+export const ID_TALENT_AVAILABILITY_OFFERS: number = 2
+export const ID_TALENT_AVAILABILITY_NOT_AVAILABLE: number = 3
+
+export const LABELS_TALENT_AVAILABILITY = ref<Record<number, string>>({
+    [ID_TALENT_AVAILABILITY_INTERVIEWS]: 'Disponible para entrevistas',
+    [ID_TALENT_AVAILABILITY_OFFERS]: 'Abierto a ofertas',
+    [ID_TALENT_AVAILABILITY_NOT_AVAILABLE]: 'No disponible',
+})
+
+export const COLORS_TALENT_AVAILABILITY = ref<Record<number, string>>({
+    [ID_TALENT_AVAILABILITY_INTERVIEWS]: 'green',
+    [ID_TALENT_AVAILABILITY_OFFERS]: 'yellow',
+    [ID_TALENT_AVAILABILITY_NOT_AVAILABLE]: 'red',
+})
