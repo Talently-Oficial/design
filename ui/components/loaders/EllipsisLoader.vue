decebad7ffb46b5d4cc85dfe275bc91b4473f7a7
dilverdev <dilvermg@gmail.com>
2024-07-16 11:40:35 -0500

    feat(ui): add component UEllipsisLoader

---
+++ ui/components/loaders/EllipsisLoader.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <span class="ellipsis-anim">
+    <span>.</span>
+    <span>.</span>
+    <span>.</span>
+  </span>
+</template>
+
+<style scoped>
+.ellipsis-anim span {
+  opacity: 0;
+  animation: ellipsis-dot 1s infinite;
+}
+
+.ellipsis-anim span:nth-child(1) {
+  animation-delay: 0.0s;
+}
+
+.ellipsis-anim span:nth-child(2) {
+  animation-delay: 0.1s;
+}
+
+.ellipsis-anim span:nth-child(3) {
+  animation-delay: 0.2s;
+}
+
+@-webkit-keyframes ellipsis-dot {
+  0% { opacity: 0; }
+  50% { opacity: 1; }
+  100% { opacity: 0; }
+}
+
+@keyframes ellipsis-dot {
+  0% { opacity: 0; }
+  50% { opacity: 1; }
+  100% { opacity: 0; }
+}
+</style>
