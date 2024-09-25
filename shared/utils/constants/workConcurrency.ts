b2c17beb076c0d4d3c834bcbf4fd482a4252fcb3
dilverdev <dilvermg@gmail.com>
2024-09-25 17:53:02 -0500

    feat(shared): add util workConcurrency

---
+++ shared/utils/constants/workConcurrency.ts
@@ -0,0 +1,18 @@
+export const LABELS_WORK_CONCURRENCY = [
+    {
+        id: 'user_high_data_high',
+        label: 'Alta concurrencia de usuarios / Alta concurrencia de datos',
+    },
+    {
+        id: 'user_low_data_high',
+        label: 'Baja concurrencia de usuarios / Alta concurrencia de datos',
+    },
+    {
+        id: 'user_high_data_low',
+        label: 'Alta concurrencia de usuarios / Baja concurrencia de datos',
+    },
+    {
+        id: 'user_low_data_low',
+        label: 'Baja concurrencia de usuarios / Baja concurrencia de datos',
+    },
+]
